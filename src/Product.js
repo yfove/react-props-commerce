adb07a24834d174365e0630706c06ea31e91b5e4
curtis chat <curtis.ch.chat@gmail.com>
2022-01-21 01:06:48 -0700

    create product component with props

---
+++ src/Product.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Product(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.description}</h2>
+      <h3>{props.description}</h3>
+    </div>
+  );
+}
+
+export default Product;
