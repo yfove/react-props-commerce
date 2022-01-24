c9fec4e2a0b80527cc474a477e61a23ee77b2e67
curtis chat <curtis.ch.chat@gmail.com>
2022-01-23 21:25:25 -0700

    item description subcomponent and pass down item description props

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,10 @@
 import React from "react";
+import ItemDescription from "./ItemDescription";
 
 function Product({ name, description, price }) {
   return (
     <div>
-      <h1>{name}</h1>
-      <h2>{description}</h2>
+      <ItemDescription name={name} description={description} />
       <h3>${price}</h3>
     </div>
   );
